81e9bbb5a533773afa18344dac538f371508ed21
Mikołaj Lewandowski <mikolevy1@gmail.com>
2020-10-05 16:57:34 +0200

    APIView example

--- users/resources.py
+++ users/resources.py
@@ -1,21 +1,41 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from users.models import Student
+from users.models import Student, CustomUser
 
 
 class StudentSerializer(serializers.Serializer):
 
-    pk = serializers.IntegerField()
+    pk = serializers.IntegerField(read_only=True)
+    email = serializers.EmailField(source="user.email")
+    username = serializers.CharField(source="user.username")
     first_name = serializers.CharField(source="user.first_name")
     last_name = serializers.CharField(source="user.last_name")
     total_points = serializers.IntegerField()
 
+    def create(self, validated_data):
+        user_data = validated_data.pop("user")
+        user = CustomUser.objects.create(**user_data)
+
+        return Student.objects.create(user=user, total_points=validated_data["total_points"])
+
+    def update(self, instance, validated_data):
+        user = instance.user
+        user_data = validated_data.get("user", {})
+        user.email = user_data.get("email", user.email)
+        user.username = user_data.get("username", user.username)
+        user.first_name = user_data.get("first_name", user.first_name)
+        user.last_name = user_data.get("last_name", user.last_name)
+        user.save()
+
+        instance.total_points = validated_data.get("total_points", instance.total_points)
+        instance.save()
+        return instance
+
 
 class StudentsResource(APIView):
-
     def get(self, request, pk=None):
         if pk is None:
             students = Student.objects.all()
@@ -24,6 +44,30 @@ class StudentsResource(APIView):
             student = Student.objects.get(pk=pk)
             serializer = StudentSerializer(student)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        student = Student.objects.get(pk=pk)
+        serializer = StudentSerializer(student, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def patch(self, request, pk):
+        student = Student.objects.get(pk=pk)
+        serializer = StudentSerializer(student, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        Student.objects.filter(pk=pk).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(["GET"])
