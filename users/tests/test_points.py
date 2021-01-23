d34f8f6c620cbca418355646a49ed014c3373dc7
Patryk Lomza <p.lomza@outlook.com>
2021-01-23 13:06:09 +0100

    Add tests for invalid payload

--- users/tests/test_points.py
+++ users/tests/test_points.py
@@ -8,7 +8,7 @@ from users.models import Point, CustomUser, Student
 def test_add_point(client):
     points = Point.objects.all()
     assert len(points) == 0
-    
+
     response = client.post(
         "/api/users/students/2/points/",
         {
@@ -24,3 +24,39 @@ def test_add_point(client):
     points = Point.objects.all()
     assert len(points) == 1
 
+@pytest.mark.django_db
+def test_add_point_invalid_json(client):
+    points = Point.objects.all()
+    assert len(points) == 0
+    
+    response = client.post(
+        "/api/users/students/2/points/",
+        {
+            "value": 1,
+            "assigner": 0,
+            "assignee": 2,  
+        },
+        content_type="application/json"
+    )
+    assert response.status_code == 400
+    
+    points = Point.objects.all()
+    assert len(points) == 0
+
+@pytest.mark.django_db
+def test_add_point_invalid_json_keys(client):
+    points = Point.objects.all()
+    assert len(points) == 0
+    
+    response = client.post(
+        "/api/users/students/2/points/",
+        {
+            "value": 1,
+            "assignee": 2,  
+        },
+        content_type="application/json"
+    )
+    assert response.status_code == 400
+    
+    points = Point.objects.all()
+    assert len(points) == 0
