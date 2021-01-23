6165e924a68f11cebb5f4c0f99dc0391a7407e32
Patryk Lomza <p.lomza@outlook.com>
2021-01-23 15:26:51 +0100

    Add tests checking GET request for single point

--- users/tests/test_points.py
+++ users/tests/test_points.py
@@ -1,7 +1,7 @@
 import json
 import pytest
 
-from users.models import Point, CustomUser, Student
+from users.models import Point, CustomUser, Student, Caregiver
 
 
 @pytest.mark.django_db
@@ -56,3 +56,16 @@ def test_add_point_invalid_json_keys(client):
     
     points = Point.objects.all()
     assert len(points) == 0
+    
+@pytest.mark.django_db
+def test_get_single_point(client):
+    assigner =  Caregiver.objects.get(id=1)
+    assignee = Student.objects.get(id=2)
+    point = Point.objects.create(value=22, assigner=assigner, assignee=assignee)
+    response = client.get(f"/api/users/students/2/points/{point.id}")
+    assert response.status_code == 200
+    assert response.data["value"] == 22
+    
+def test_get_single_point_incorrect_id(client):
+    response = client.get(f"/api/users/students/2/points/foo")
+    assert response.status_code == 404
