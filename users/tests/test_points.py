8b5d28a481f5dbe3d92acb96d104e7392a7a7030
Patryk Lomza <p.lomza@outlook.com>
2021-01-25 21:53:50 +0100

    Add test for getting points of a given student

--- users/tests/test_points.py
+++ users/tests/test_points.py
@@ -71,3 +71,16 @@ def test_get_single_point(client):
 def test_get_single_point_incorrect_id(client):
     response = client.get(f"/api/users/students/2/points/foo")
     assert response.status_code == 404
+
+@pytest.mark.django_db
+def test_get_points_for_filtered_student(client):
+    assigner =  Caregiver.objects.get(pk=1)
+    assignee = Student.objects.get(pk=2)
+    point_1 = Point.objects.create(value=22, assigner=assigner, assignee=assignee)
+    point_2 = Point.objects.create(value=17, assigner=assigner, assignee=assignee)
+
+    response = client.get(f"/api/users/points/?userId={assignee.pk}")
+
+    assert response.status_code == 200
+    assert response.data[0]["value"] == 22
+    assert response.data[1]["value"] == 17
