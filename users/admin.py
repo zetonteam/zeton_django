6ee6bea1b90e4bbaeecb8580993c331e246de3dc
Leszek Miotk <tgb.dc7@gmail.com>
2021-02-15 20:24:51 +0100

    register caregiver admin model

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, Caregiver
 
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ['first_name', 'last_name', 'email', 'is_auth', ]
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
+@admin.register(Caregiver)
+class CaregiverAdmin(admin.ModelAdmin):
+    list_display = ['user']
