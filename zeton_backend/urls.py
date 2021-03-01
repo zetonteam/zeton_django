acc0bdde75567d3ca1d6b0d76d6f4fe3c5e93c52
Patryk Lomza <p.lomza@outlook.com>
2021-03-01 11:23:05 +0100

    Add swagger endpoint url

--- zeton_backend/urls.py
+++ zeton_backend/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
+from rest_framework.schemas import get_schema_view
 
 api_urls = [
     path("users/", include("users.urls")),
@@ -7,5 +9,14 @@ api_urls = [
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include(api_urls))
+    path("api/", include(api_urls)),
+    path("openapi", get_schema_view(
+        title="Zeton project",
+        description="API for Zeton React client",
+        version="0.0.1",
+    ), name='openapi-schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
 ]
