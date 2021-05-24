c3e6085b7479dac0b2c4afb9d2be8df6825f28c8
Leszek Miotk <tgb.dc7@gmail.com>
2021-05-24 20:35:34 +0200

    register models in admin panel

--- users/admin.py
+++ users/admin.py
@@ -3,17 +3,22 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Caregiver, Student
 
 
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
+# @admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'email', 'is_auth', ]
+    # inlines = [CaregiverAdminInline, ]
 
 
 @admin.register(Caregiver)
 class CaregiverAdmin(admin.ModelAdmin):
+    model = Caregiver
     list_display = ['user']
 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['user', 'total_points']
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+# admin.site.register(Caregiver)
