1da811311d25c070f6840d578ab812c23f9f4eb1
Py Explorer <pyexplorer@hs3.pl>
2024-01-10 21:05:44 +0100

    session-004: WIP - add permission module

--- users/resources.py
+++ users/resources.py
@@ -1,5 +1,5 @@
 from django.http.response import Http404
-from rest_framework import status, generics
+from rest_framework import status, generics, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
--- users/serializers.py
+++ users/serializers.py
@@ -103,6 +103,14 @@ class CustomUserSerializerWithToken(serializers.ModelSerializer):  # Handling Re
 
 
 class PointSerializer(serializers.ModelSerializer):
+
+    """
+    TODO
+        we need to check assigner has permission to add points for student/'assignee'
+        add points to students total_points
+
+
+    """
     class Meta:
         model = Point
         fields = ('pk', 'value', 'assigner', 'assignee', 'assignment_date')
