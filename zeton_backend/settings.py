ef80b38c5702e342b462e1efb3a5a728df38a6ed
Py Explorer <pyexplorer@hs3.pl>
2024-03-13 19:16:43 +0100

    Session 012: Add database logging

--- zeton_backend/settings.py
+++ zeton_backend/settings.py
@@ -155,3 +155,25 @@ SPECTACULAR_SETTINGS = {
         "persistAuthorization": True
     },
 }
+
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
