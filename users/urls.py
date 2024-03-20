230a2a90d23f9704c0090621c3c349048aeb2042
Szymon Mikitiuk <singlet.bunches_0m@icloud.com>
2024-03-20 19:53:17 +0100

    feat: introduce unform code formatting with ruff 💪💪💪

--- users/urls.py
+++ users/urls.py
@@ -1,25 +1,23 @@
 from django.urls import path
-
 from rest_framework_jwt.views import obtain_jwt_token
 
-from users.resources import StudentsResource, PrizesResource, TasksResource, PointResource
-
-from users.views import current_user, UserList
+from users.resources import (
+    PointResource,
+    PrizesResource,
+    StudentsResource,
+    TasksResource,
+)
+from users.views import UserList, current_user
 
 urlpatterns = [
-    path('token-auth/', obtain_jwt_token),
-
-    path('current-user/', current_user),
-    path('register/', UserList.as_view()),
-
+    path("token-auth/", obtain_jwt_token),
+    path("current-user/", current_user),
+    path("register/", UserList.as_view()),
     path("students/<int:pk>/points/", PointResource.as_view(), name="points-resource"),
     path("students/<int:pk>/", StudentsResource.as_view(), name="student-resource"),
     path("students/", StudentsResource.as_view(), name="students-resource"),
-
     path("prizes/<int:pk>/", PrizesResource.as_view(), name="prize-resource"),
     path("prizes/", PrizesResource.as_view(), name="prizes-resource"),
-
     path("tasks/<int:pk>/", TasksResource.as_view(), name="task-resource"),
     path("tasks/", TasksResource.as_view(), name="tasks-resource"),
-
 ]
--- zeton_backend/utils.py
+++ zeton_backend/utils.py
@@ -3,6 +3,6 @@ from users.serializers import CustomUserSerializer
 
 def my_jwt_response_handler(token, user=None, request=None):
     return {
-        'token': token,
-        'user': CustomUserSerializer(user, context={'request': request}).data
+        "token": token,
+        "user": CustomUserSerializer(user, context={"request": request}).data,
     }
